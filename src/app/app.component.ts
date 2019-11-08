18753a7e66dae0513657bd2940c95d576eab1af9
James Busbee <jsb1018@sru.edu>
2019-11-07 23:48:22 -0500

    created objects for jobs and skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume';
+  bodyObjects = {
+    name: 'James Busbee',
+    age: 22,
+    jobs: [
+      {
+        title: 'Systems Engineering Internship',
+        place: 'American Eagle Outfitters',
+        date: 'June 2019-August 2019'
+      },
+      {
+        title: 'Food Prep Chef/Dishwasher',
+        place: 'Goodfellas Drafthouse',
+        date: 'May 2016-August 2018'
+      }
+    ],
+    skills: [
+      {
+        webDev: 'AJAX, Angular, Bootsrap, PHP',
+        databases: 'Excel, SQL, XML',
+        programming: 'C++, Bash Scripting, Python',
+        hardware: 'familiar with computer system hardware'
+      }
+    ]
+  };
 }
